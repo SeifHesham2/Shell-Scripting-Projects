4d63d81ef16c46d198a366960f4cbab6e33a7ad4
SeifHesham2 <99514442+SeifHesham2@users.noreply.github.com>
2024-04-15 22:48:16 +0200

    Update show-attackers.sh

--- Scripts/show-attackers.sh
+++ Scripts/show-attackers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Make sure a file was supplied as an argument.
 if [[ ${#} -ne 1 ]]
 then
    echo "Usage: ${0} <Filename>" >&2
@@ -12,13 +12,14 @@ then
    echo "The file does not exist." >&2
    exit 1
 fi
-
+# Display the CSV header.
 echo 'Count , IP , Location' 
 
-
+# Loop through the list of failed attempts and corresponding addresses.
 grep Failed ${FILE} | awk -F 'from' '{print $2 }' | awk '{print $1 }' | sort | uniq -c | sort -nr | while read COUNT IP
 do
-  if [[ ${COUNT} -gt 10 ]]
+# If the number of failed attempts is greater than the limit, display count, IP, and location.
+if [[ ${COUNT} -gt 10 ]]
    then
      LOCATION=$(geoiplookup ${IP}| awk -F ', ' '{print $2}')
      echo "${COUNT},${IP},${LOCATION}"
